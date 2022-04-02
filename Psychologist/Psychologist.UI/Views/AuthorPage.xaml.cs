58f017545bc728e277b1536c732754bb98f2adec
Sherafkan <sherafkan@silkroadprofessionals.com>
2022-04-02 09:21:09 +0500

    bug fix 2

--- Psychologist/Psychologist.UI/Views/AuthorPage.xaml.cs
+++ Psychologist/Psychologist.UI/Views/AuthorPage.xaml.cs
@@ -1,23 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
 using Psychologist.Core;
 using Psychologist.Core.ViewModels;
-using Xamarin.Forms;
+using Xamarin.Essentials;
 using Xamarin.Forms.Xaml;
 
 namespace Psychologist.UI.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AuthorPage 
+    public partial class AuthorPage
     {
         public AuthorPage()
         {
             InitializeComponent();
             BindingContext = ViewModel = DependencyInitializerCore.Container.Resolve<AuthorViewModel>();
+        }
+
+        private async void EmailTapGesture(object sender, EventArgs e)
+        {
+            try
+            {
+                await Email.ComposeAsync(string.Empty, string.Empty, "shukhratjoni@gmail.com");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private void PhoneTapGesture(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("+992927772445");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private async void InstagramTapGesture(object sender, EventArgs e)
+        {
+            try
+            {
+                await Launcher.OpenAsync("https://www.instagram.com/shukhratjoni/");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
         }
     }
 }
