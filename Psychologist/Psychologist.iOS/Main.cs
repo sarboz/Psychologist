80bceefcdccc006bb974ecfb2b27508286bbced1
Bohirjon <bohirjon.a@gmail.com>
2022-05-05 13:33:31 +0500

    ios issue fixed

--- Psychologist/Psychologist.iOS/Main.cs
+++ Psychologist/Psychologist.iOS/Main.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+[assembly: Preserve(typeof(System.Linq.Queryable), AllMembers = true)]
 namespace Psychologist.UI.iOS
 {
     public class Application
